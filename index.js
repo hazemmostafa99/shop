91488fbe2f310bd017c6453931b4f64f2f900709
hazemmostafa99 <hazem77mostafa90@gmail.com>
2023-04-08 08:07:13 +0200

    index.js

--- index.js
+++ index.js
@@ -12,19 +12,17 @@ const cors = require("cors");
 
 dotenv.config();
 
-const connectDB = async () => {
-  try {
-    const conn = await mongoose
-      .connect(process.env.MONGO_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        writeConcern: { w: "majority", j: true, wtimeout: 1000 },
-      })
-      .then(() => console.log("DB Connection Successfull!"));
-  } catch (error) {
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    writeConcern: { w: "majority", j: true, wtimeout: 1000 },
+  })
+  .then(() => console.log("DB Connection Successfull!"))
+  .catch((err) => {
     console.log(err);
-  }
-};
+  });
 
 app.use(cors());
 app.use(express.json());
@@ -35,8 +33,6 @@ app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
 app.use("/api/checkout", stripeRoute);
 
-connectDB().then(() => {
-  app.listen(process.env.PORT || 5000, () => {
-    console.log("Backend server is running!");
-  });
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Backend server is running!");
 });
