fbc7b220560390561e60d28635920683e6e5da82
hazemmostafa99 <hazem77mostafa90@gmail.com>
2023-04-08 08:51:09 +0200

    conection

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ dotenv.config();
 
 
 mongoose
-  .connect(process.env.MONGO_URL, {
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     writeConcern: { w: "majority", j: true, wtimeout: 1000 },
